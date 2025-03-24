f102b6c584cb69cc89dce621d0911bf66fc49dc1
Igor Brito <igoriago43@hotmail.com>
2025-03-23 23:11:00 -0300

    Correções de validação ao adicionar produtos Null validação de quantidade e de preço.

--- MauiAppMinhasCompras/Models/Produto.cs
+++ MauiAppMinhasCompras/Models/Produto.cs
@@ -4,12 +4,49 @@ namespace MauiAppMinhasCompras.Models
 {
     public class Produto
     {
+        string _descricao;
+        double _quantidade;
+        double _preco;
+
+
+
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
-        public string Descricao { get; set; }
-        public double Quantidade { get; set; }
+        public string Descricao {
+            get => _descricao;
+            set 
+            {
+                if(value == null)
+                {
+                    throw new Exception("Por favor, preencha a descrição. ");
+                }
 
-        public double Preco { get; set; }
+                _descricao = value;
+            } 
+        }
+        public double Quantidade { 
+            get => _quantidade; 
+            set
+            {
+                if(value == 0)
+                {
+                    throw new Exception("Por favor, preencha um número válido maior que 0");
+                }
+            }
+                
+        }
+
+        public double Preco
+        {
+            get => _preco;
+            set
+            {
+                if (value == 0)
+                {
+                    throw new Exception("Por favor, preencha um número válido maior que 0");
+                }
+            }
+        }
         public double Total { get => Quantidade * Preco; }
 
 
